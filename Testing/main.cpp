cf2d0bde282a3435c7235d5f320f5f569cce29e0
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-03-22 15:08:24 +0100

    Testing - Basic demo app
    This demo app demonstrates usage of config, window and level.

--- Testing/main.cpp
+++ Testing/main.cpp
@@ -6,48 +6,48 @@
 #pragma comment(lib, "dgm.lib")
 #endif
 
-const int FPS = 40;
-const int WIDTH = 1280;
-const int HEIGHT = 720;
-
-class ExceptState : public dgm::Molded::AppState {
-protected:
-	std::vector<int> values;
-
-public:
-	// Inherited via AppState
-	virtual bool init() override {
-		try {
-			values[10] = 11;
-		}
-		catch (...) {
-			return false;
-		}
-
-		return true;
-	}
-
-	virtual void draw() override {
-	}
-
-	virtual void update() override {
-	}
-
-	virtual void input() override {
-	}
-
-	ExceptState(dgm::Molded::App *app) {
-		dgm::Molded::AppState::app = app;
-	}
-};
-
 /* This is just an internal testing sandbox */
 int main(int argc, char *argv[]) {
-	dgm::Molded::App app;
-	app.init(sf::VideoMode(WIDTH, HEIGHT), "Sandbox", sf::Style::Default);
+	dgm::Config config;
+	if (not config.loadFromFile("../Data/config.ini")) return 1;
 
-	app.pushState(new ExceptState(&app));
-	app.loop();
+	dgm::Window window;
+	window.open(config);
+
+	std::vector<int> imageData = {
+		0, 1, 2, 1, 0,
+		1, 2, 3, 2, 1,
+		1, 2, 3, 2, 1,
+		1, 2, 3, 2, 1,
+		0, 1, 2, 1, 0
+	};
+	dgm::Clip clip({ 64,64 }, { 0,0,256,64 });
+	sf::Texture texture;
+	if (not texture.loadFromFile("../Graphics/tileset.png")) return 2;
+
+	dgm::Level level;
+	level.getRenderer().build(clip, { 64,64 }, imageData, {5, 5});
+	level.getRenderer().setTexture(&texture);
+
+	sf::Event event;
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		/* LOGIC */
+
+		/* DRAW */
+		window.beginDraw();
+
+		level.draw(window);
+
+		window.endDraw();
+	}
+
+	config.saveToFile("../Data/config.ini");
 
 	return 0;
 }
