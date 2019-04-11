d1978237e62cb64b6bcf1510fee79a7798ab1144
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-04-11 16:04:30 +0200

    [MAINT] Window inline what can be inlined

--- DGM/include/DGM/classes/Window.hpp
+++ DGM/include/DGM/classes/Window.hpp
@@ -3,6 +3,11 @@
 #include <DGM\dgm.hpp>
 
 namespace dgm {
+	/**
+	 *  \brief Wrapper around sf::RenderWindow
+	 *
+	 *  Class provides method for easier window management.
+	 */
 	class Window {
 	protected:
 		sf::RenderWindow window;
@@ -13,8 +18,8 @@ namespace dgm {
 
 	public:
 		void open(const cfg::Ini &config);
-		void open(const sf::Vector2u &resolution, const std::string &title, const bool fullscreen);
-		void close();
+		void open(const sf::Vector2u &resolution, const std::string &title, bool fullscreen);
+		void close() { window.close(); }
 		void close(cfg::Ini &config);
 		bool pollEvent(sf::Event &event) { return window.pollEvent(event); }
 		void toggleFullscreen();
@@ -26,11 +31,13 @@ namespace dgm {
 		sf::RenderWindow &getWindowContext() { return window; }
 		const sf::RenderWindow &getWindowContext() const { return window; }
 
-		void beginDraw(const sf::Color &color = sf::Color::Black);
-		void draw(sf::Drawable &drawable);
-		void endDraw();
+		void beginDraw(const sf::Color &color = sf::Color::Black) { window.clear(color); }
+		void draw(sf::Drawable &drawable) { window.draw(drawable); }
+		void endDraw() { window.display(); }
 
 		Window() {}
+		Window(const cfg::Ini &config) { open(config); }
+		Window(const sf::Vector2u &resolution, const std::string &title, bool fullscreen) { open(resolution, title, fullscreen); }
 		~Window() {}
 	};
 };
