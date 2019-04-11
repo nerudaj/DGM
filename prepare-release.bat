38f208c18dceba618be2de46eac81d7beceeccf6
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-04-11 21:21:27 +0200

    [MAINT] Add prepare-release script

---
+++ prepare-release.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set /p version=<VERSION
+
+echo Making release folders
+mkdir DGM-%version%\include
+mkdir DGM-%version%\lib
+# TODO: docs
+
+copy vsbuild\Debug\dgm-d.lib DGM-%version%\lib
+copy vsbuild\Release\dgm.lib DGM-%version%\lib
+robocopy /S include DGM-%version%\include
+robocopy 3rdParty\include DGM-%version%\include Config.hpp Args.hpp Item.hpp Csv.hpp Ini.hpp Logger.hpp Strings.hpp
+
+@echo on
