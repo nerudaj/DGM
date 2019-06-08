4158244388acef66393ec68ea86291416ad8dfff
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-06-08 22:33:32 +0200

    [MAINT] Bump to newer version of dshlibs

--- 3rdParty/bootstrap.bat
+++ 3rdParty/bootstrap.bat
@@ -1,14 +1,14 @@
 @echo off
 
+echo Downloading dshlibs
+cscript C:\tools\bin\wget.js "https://github.com/nerudaj/dsh/releases/download/v0.2.3/dshlibs-0.2.3.zip" dshlibs.zip
+cscript C:\tools\bin\unzip.vbs dshlibs.zip
+move dshlibs-0.2.3 dshlibs
+
 echo Downloading SFML
 cscript C:\tools\bin\wget.js "https://www.sfml-dev.org/files/SFML-2.5.1-windows-vc15-32-bit.zip" sfml.zip
 cscript C:\tools\bin\unzip.vbs sfml.zip
 move SFML-2.5.1 SFML
-
-echo Downloading dshlibs
-cscript C:\tools\bin\wget.js "https://github.com/nerudaj/dsh/releases/download/dshlibs_v0.1.1/dshlibs-0.1.1.zip" dshlibs.zip
-cscript C:\tools\bin\unzip.vbs dshlibs.zip
-move dshlibs-0.1.1 dshlibs
 
 del sfml.zip dshlibs.zip
 
