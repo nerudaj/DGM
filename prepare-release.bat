cd5af32f0a39e7ee4226d5656a96b6eb544ab509
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-07-25 00:19:21 +0200

    [MAINT] Update prepare-release script

--- prepare-release.bat
+++ prepare-release.bat
@@ -1,16 +1,40 @@
+# Get path to cmake
+call /tools/doomsh.cmd
+
 @echo off
 
-set /p version=<VERSION
+echo Phase 1 - Purging
+rd /s /q vsbuild
 
-echo Making release folders
+echo Phase 2 - Configuring
+mkdir vsbuild
+cd vsbuild
+cmake.exe ..
+cd ..
+
+echo Phase 3 - Building
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+cd vsbuild
+devenv dgm.sln /Build Debug
+devenv dgm.sln /Build Release
+cd ..
+
+echo Phase 4 - Release filesys
+set /p version=<VERSION
 mkdir DGM-%version%\include
 mkdir DGM-%version%\lib
-# TODO: docs
 
-echo Copying data
+echo Phase 5 - Copying data
+copy changelog.txt DGM-%version%
 copy vsbuild\Debug\dgm-d.lib DGM-%version%\lib
 copy vsbuild\Release\dgm.lib DGM-%version%\lib
 robocopy /S include DGM-%version%\include
 robocopy 3rdParty\dshlibs\include DGM-%version%\include Config.hpp Args.hpp Item.hpp Csv.hpp Ini.hpp Logger.hpp Strings.hpp
+
+echo Phase 6 - Project template
+mkdir DGM-Project-Template-%version%
+robocopy /S template DGM-Project-Template-%version%
+
+echo Done!
 
 @echo on
