694af16ebdaa0f3bf5ce5710ba48bd58cb5d6451
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-07-28 17:50:43 +0200

    [FEATURE] Implement barebones Options page for template

--- template/stuff/AppStateMenuOptions.cpp
+++ template/stuff/AppStateMenuOptions.cpp
@@ -1,17 +1,68 @@
 #include "AppStateMenuOptions.hpp"
 
+void AppStateMenuOptions::processButtonClick(int id) {
+	switch (id) {
+	case Back:
+		app->popState();
+		break;
+	}
+}
+
 void AppStateMenuOptions::input() {
+	mousePos = sf::Mouse::getPosition(app->window.getWindowContext());
+
+	sf::Event event;
+	while (app->window.pollEvent(event)) {
+		if (event.type == sf::Event::MouseButtonPressed) {
+			if (chkboxFullscreen.isHighlighted(mousePos)) {
+				chkboxFullscreen.toggle();
+				app->window.toggleFullscreen();
+			}
+			else if (buttonBack.isHighlighted(mousePos)) {
+				processButtonClick(buttonBack.getId());
+			}
+		}
+	}
 }
 
 void AppStateMenuOptions::update() {
+	chkboxFullscreen.update(mousePos);
+	buttonBack.update(mousePos);
 }
 
 void AppStateMenuOptions::draw() {
 	app->window.beginDraw();
 
+	app->window.draw(title);
+	app->window.draw(labelFullscreen);
+	chkboxFullscreen.draw(app->window);
+	buttonBack.draw(app->window);
+
 	app->window.endDraw();
 }
 
 bool AppStateMenuOptions::init() {
-	return false;
+	sf::Font &font = resmgr.get<sf::Font>("cruft.ttf");
+
+	title.setCharacterSize(72);
+	title.setFillColor(sf::Color::White);
+	title.setFont(font);
+	title.setString("Options");
+
+	auto bounds = title.getGlobalBounds();
+	sf::Vector2f textSize = { bounds.width, bounds.height };
+	auto windowSize = app->window.getSize();
+	title.setPosition((sf::Vector2f(windowSize / 2u) - (textSize / 2.f)).x, 20);
+
+	labelFullscreen.setFont(font);
+	labelFullscreen.setString("Fullscreen");
+	labelFullscreen.setCharacterSize(32);
+	labelFullscreen.setPosition(50.f, 200.f);
+	labelFullscreen.setFillColor(sf::Color::White);
+
+	chkboxFullscreen = gui::Checkbox({ 1000.f, 200.f }, { 32.f, 32.f }, app->window.isFullscreen(), 0);
+
+	buttonBack = gui::Button({1050, 630}, { 180, 40 }, "Back", font, Back);
+
+	return true;
 }
--- template/stuff/AppStateMenuOptions.hpp
+++ template/stuff/AppStateMenuOptions.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
 #include <DGM/dgm.hpp>
+#include "Gui.hpp"
 
 class AppStateMenuOptions : public dgm::AppState {
+protected:
+	sf::Text title;
+	sf::Text labelFullscreen;
+	gui::Button buttonBack;
+	gui::Checkbox chkboxFullscreen;
+
+	sf::Vector2i mousePos;
+	const dgm::ResourceManager &resmgr;
+
+	void processButtonClick(int id);
+
 public:
+	enum {
+		Back
+	};
+
 	virtual void input() override;
 	virtual void update() override;
 	virtual void draw() override;
 	virtual bool init() override;
+
+	AppStateMenuOptions(const dgm::ResourceManager &resmgr) : resmgr(resmgr) {}
 };
