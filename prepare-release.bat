2688882a37f34c0454f544f7883d88e7ff9a3cf5
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-09-07 18:50:38 +0200

    Update prepare-release.bat

--- prepare-release.bat
+++ prepare-release.bat
@@ -42,6 +42,16 @@ echo Phase 6 - Project template
 mkdir RELEASE\DGM-Project-Template-%version%
 robocopy /S template RELEASE\DGM-Project-Template-%version%
 
+rem Cleanup auxiliary template files
+cd RELEASE\DGM-Project-Template-%version%
+rmdir /s /q vsbuild
+rmdir /s /q 3rdParty/TGUI
+rmdir /s /q 3rdParty/DGM
+rmdir /s /q 3rdParty/SFML
+rmdir /s /q RELEASE
+del app.ini
+cd ../..
+
 echo Phase 7 - Generating doxydocs
 cd docs
 doxygen
