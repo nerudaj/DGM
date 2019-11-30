50e080736654a9811014db20fae514d0a0e36d90
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-11-30 22:32:15 +0100

    [FEATURE] Download dependencies on configure

---
+++ dependencies.cmake
@@ -0,0 +1,7 @@
+set ( DSH_VERSION  "1.0.0" )
+set ( SFML_VERSION "2.5.1" )
+set ( JSON_VERSION "3.7.3" )
+
+set ( DSH_URL "https://github.com/nerudaj/dsh/releases/download/v${DSH_VERSION}/dsh-${DSH_VERSION}-VSx64.zip.zip" )
+set ( SFML_URL "https://github.com/SFML/SFML/releases/download/${SFML_VERSION}/SFML-${SFML_VERSION}-windows-vc15-64-bit.zip" )
+set ( JSON_URL "https://github.com/nlohmann/json/releases/download/v${JSON_VERSION}/json.hpp" )
