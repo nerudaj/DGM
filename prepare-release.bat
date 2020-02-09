8336028e6b70059cd334defe2b098debb3b5ccb0
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2020-02-09 14:34:35 +0100

    Update prepare-release.bat

--- prepare-release.bat
+++ prepare-release.bat
@@ -7,7 +7,7 @@ call /tools/doomsh.cmd
 
 echo Phase 1 - Purging
 rd /s /q %BUILDDIR%
-rd /s /q project-template/%BUILDDIR%
+rd /s /q project-template\%BUILDDIR%
 
 echo Phase 2 - Configuring
 mkdir %BUILDDIR%
