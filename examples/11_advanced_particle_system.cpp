09f74889ddb0c07f480b1168a806e39830c7b32d
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2020-02-23 15:47:32 +0100

    INDEV sand particle system

--- examples/11_advanced_particle_system.cpp
+++ examples/11_advanced_particle_system.cpp
@@ -15,11 +15,8 @@ public:
 
 	void spawn(const sf::Vector2f &position) override {
 		// NOTE: All of initialization must happen here
-
-		size = { 2.f, 2.f }; // Size must be set prior to Partile::spawn
-		body.setRadius(1.f);
-
-		body.setPosition(position);
+		body.setRadius(size.x / 2.f);
+		body.setPosition(position + size / 2.f);
 		Particle::spawn(position);
 
 		for (unsigned i = 0; i < 4; i++) {
@@ -31,26 +28,51 @@ public:
 class SandParticleFactory : public dgm::ps::ParticleFactoryInterface {
 public:
 	virtual void reset() override {}
-	virtual dgm::ps::Particle * create() const override { return new SandParticle; }
+	virtual dgm::ps::Particle * create() override { return new SandParticle; }
 };
+
+float relativeVectorSize(const sf::Vector2f& vec) {
+	return vec.x * vec.x + vec.y * vec.y;
+}
 
 class SandSystem : public dgm::ps::ParticleSystemInterface {
 private:
+	const float X_VELOCITY = 10.f;
+	const float PARTICLE_RADIUS = 1.f;
+	std::vector<std::vector<SandParticle*>> nearestNeighbours;
+
 	sf::Time spawnTimer = sf::seconds(0.f);
 
-	unsigned particleCollidesWithOther(unsigned i) {
-		SandParticle *first = dynamic_cast<SandParticle*>(particles[i]);
-
-		for (unsigned j = 0; j < particles.size(); j++) {
-			if (i == j) continue;
-			SandParticle *second = dynamic_cast<SandParticle*>(particles[j]);
-
-			if (dgm::Collision::basic(first->body, second->body)) {
-				return j;
-			}
+	void updateNearestNeighboursMatrix(float deltaTime) {
+		for (auto& vec : nearestNeighbours) {
+			vec.clear();
+			vec.reserve(30);
 		}
 
-		return i;
+		float maxDistance = getMaxDistance(deltaTime);
+
+		for (unsigned i = 0; i < particles.size(); i++) {
+			auto position = particles[i]->getPosition();
+
+			for (unsigned j = i + 1; j < particles.size(); j++) {
+				auto otherPos = particles[j]->getPosition();
+
+				if (relativeVectorSize(position - otherPos) <= maxDistance) {
+					nearestNeighbours[i].push_back(dynamic_cast<SandParticle*>(particles[j]));
+					nearestNeighbours[j].push_back(dynamic_cast<SandParticle*>(particles[i]));
+				}
+			}
+		}
+	}
+
+	/**
+	 *  Get maximum distance between two particles that
+	 *  both can traverse in a single frame in order to collide
+	 */
+	float getMaxDistance(float deltaTime) const {
+		auto maxMovement = sf::Vector2f(X_VELOCITY, gravity.y) * deltaTime;
+		sf::Vector2f particleSize(PARTICLE_RADIUS * 2.f, PARTICLE_RADIUS * 2.f);
+		return relativeVectorSize((maxMovement + particleSize) * 2.f);
 	}
 
 public:
@@ -67,41 +89,55 @@ public:
 			spawnTimer -= SPAWN_TIMEOUT;
 
 			if (particles.expand()) {
+				particles.last()->size = sf::Vector2f(1.f, 1.f) * 2.f * PARTICLE_RADIUS;
 				particles.last()->spawn(emitterPosition);
 				particles.last()->changeFrame(sf::IntRect(0, 0, 10, 10));
-/*				particles.last()->forward.x = left ? -10.f : 10.f;
-				left = !left;*/
-			}
-		}		
-
-		for (unsigned i = 0; i < particles.size(); i++) {
-			SandParticle *particle = dynamic_cast<SandParticle*>(particles[i]);
-
-			auto currentForward = (particle->forward + gravity) * time.getDeltaTime();
-
-			particle->move(currentForward);
-
-			if (dgm::Collision::basic(*level, particle->body)) {
-				particle->move(-currentForward);
-				continue;
-			}
-
-			unsigned ptr;
-			if ((ptr = particleCollidesWithOther(i)) != i) {
-				particle->move(-currentForward);
-
-				particle->forward.x = (particles[i]->getPosition() - particles[ptr]->getPosition()).x >= 0.f ? 10.f : -10.f;
-
-				particle->move(particle->forward * time.getDeltaTime());
-
-				if ((ptr = particleCollidesWithOther(i)) != i) {
-					particle->move(-particle->forward * time.getDeltaTime());
-				}
-			}
-			else {
-				particle->forward *= 0.5f;
 			}
 		}
+
+		updateNearestNeighboursMatrix(time.getDeltaTime());
+
+		for (unsigned i = 0; i < particles.size(); i++) {
+			auto* particle = dynamic_cast<SandParticle*>(particles[i]);
+			auto body = particle->body;
+			auto actualGravity = gravity * time.getDeltaTime();
+			sf::Vector2f forward(0.f, gravity.y);
+			body.move(actualGravity);
+
+			auto &nearest = nearestNeighbours[i];
+
+			for (auto other : nearest) {
+				if (dgm::Collision::basic(body, other->body)) {
+					body.move(-actualGravity);
+					forward.y = 0.f;
+
+					float myX = particle->getPosition().x;
+					float otherX = other->getPosition().x;
+					particle->forward.x = myX - otherX > 0 ? X_VELOCITY : -X_VELOCITY;
+				}
+			}
+
+			forward.x = particle->forward.x;
+			auto actualSideward = particle->forward * time.getDeltaTime();
+			body.move(actualSideward);
+			for (auto other : nearest) {
+				if (dgm::Collision::basic(body, other->body)) {
+					body.move(-actualSideward);
+					forward.x = 0.f;
+				}
+			}
+
+			forward *= time.getDeltaTime();
+			dgm::Collision::advanced(*level, particle->body, forward);
+			particle->move(forward);
+			float sign = particle->forward.x < 0.f ? -1.f : 1.f;
+			particle->forward.x = sign * dgm::Math::clamp(sign * particle->forward.x - 20.f * time.getDeltaTime(), 0.f, X_VELOCITY);
+		}
+	}
+
+	virtual bool init(const std::size_t particleCount, const dgm::Clip& clip, dgm::ps::ParticleFactoryInterface* factory) override {
+		nearestNeighbours.resize(particleCount);
+		return ParticleSystemInterface::init(particleCount, clip, factory);
 	}
 };
 
@@ -124,7 +160,7 @@ int main(int argc, char *argv[]) {
 
 	SandSystem sandSystem;
 	if (!sandSystem.init(250, dgm::Clip(), new SandParticleFactory())) return 1;
-	sandSystem.gravity = { 0.f, 50.f };
+	sandSystem.gravity = { 0.f, 100.f };
 	sandSystem.emitterPosition = { 150.f, 40.f };
 	sandSystem.level = &(level.getMesh());
 	time.reset();
